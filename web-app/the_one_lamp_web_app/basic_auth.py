a552e848befe09c4970d8bde903aa39fb72702b5
Mateusz Krainski <mateusz@krainski.eu>
2023-10-14 13:13:04 +0200

    Tweaks to basic auth
    Signed-off-by: Mateusz Krainski <mateusz@krainski.eu>

--- web-app/the_one_lamp_web_app/basic_auth.py
+++ web-app/the_one_lamp_web_app/basic_auth.py
@@ -1,3 +1,4 @@
+import os
 import secrets
 from typing import Annotated
 
@@ -7,19 +8,21 @@ from fastapi.security import HTTPBasic, HTTPBasicCredentials
 app = FastAPI()
 
 security = HTTPBasic()
+USERNAME = os.getenv("BASIC_AUTH_USERNAME", "").encode("utf8")
+PASSWORD = os.getenv("BASIC_AUTH_PASSWORD", "").encode("utf8")
+
+if USERNAME == b"" or PASSWORD == b"":
+    raise Exception(
+        "Invalid configuration, BASIC_AUTH_USERNAME and BASIC_AUTH_PASSWORD env "
+        "variables have to be set"
+    )
 
 
 def validate_user(credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
     current_username_bytes = credentials.username.encode("utf8")
-    correct_username_bytes = b"stanleyjobson"
-    is_correct_username = secrets.compare_digest(
-        current_username_bytes, correct_username_bytes
-    )
+    is_correct_username = secrets.compare_digest(current_username_bytes, USERNAME)
     current_password_bytes = credentials.password.encode("utf8")
-    correct_password_bytes = b"swordfish"
-    is_correct_password = secrets.compare_digest(
-        current_password_bytes, correct_password_bytes
-    )
+    is_correct_password = secrets.compare_digest(current_password_bytes, PASSWORD)
     if not (is_correct_username and is_correct_password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
