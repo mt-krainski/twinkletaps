d519ae46d4a09e253457d1989c7c5fe0fb3f8eda
Mateusz Krainski <mateusz@krainski.eu>
2024-03-29 21:51:02 +0100

    Add page title
    Signed-off-by: Mateusz Krainski <mateusz@krainski.eu>

--- ui/src/router/index.ts
+++ ui/src/router/index.ts
@@ -1,17 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+const defaultPageName = 'TwinkleTaps';
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'TwinkleTaps',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/HomeView.vue'),
-    }
+    },
   ],
+});
+
+router.beforeEach((to, from, next) => {
+  if (typeof to.name === 'string') {
+    document.title = to.name;
+  } else {
+    document.title = defaultPageName;
+  }
+  next();
 });
 
 export default router;
