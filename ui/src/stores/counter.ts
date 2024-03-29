2f61e4046ae72b34cede513dcc20e3add7f1cfc6
Mateusz Krainski <mateusz@krainski.eu>
2024-03-29 17:59:33 +0100

    Add Vue.js based UI
    Signed-off-by: Mateusz Krainski <mateusz@krainski.eu>

---
+++ ui/src/stores/counter.ts
@@ -0,0 +1,5 @@
+import { defineStore } from 'pinia';
+
+export const useTestStore = defineStore('test', () => {
+  return test;
+});
---
+++ ui/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="d-flex align-center flex-column">
+    <v-card class="mx-auto mt-12" width="400" variant="tonal">
+      <p class="text-center pa-3">
+        Twinkle Taps
+      </p>
+    </v-card>
+  </div>
+</template>
+
+<style></style>
+
+<script setup lang="ts">
+
+</script>
